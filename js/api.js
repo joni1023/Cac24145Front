25bc80b7a2a6c7a6e925d05c664dbd2837e003ac
joni1023 <jonfu1023@gmail.com>
2024-05-29 23:04:07 -0300

    comienzo api

---
+++ js/api.js
@@ -0,0 +1,10 @@
+const options = {method: 'GET', headers: {accept: 'application/json',
+Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTJjYTAwZDYxZWIzOTEyYjZlNzc4MDA4YWQ3ZmNjOCIsInN1YiI6IjYyODJmNmYwMTQ5NTY1MDA2NmI1NjlhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4MJSPDJhhpbHHJyNYBtH_uCZh4o0e3xGhZpcBIDy-Y8'
+}};
+
+function consumirApi(){
+    fetch('https://api.themoviedb.org/3/authentication', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+}
