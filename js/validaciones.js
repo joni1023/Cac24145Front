22af99bddba426251a10fe60761a5b4e946250b1
Daniel D'Onofrio <danieldonofrio@live.com>
2024-05-30 21:47:17 -0300

    cambios en validaciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -39,14 +39,16 @@ function validarLogin() {
 
 function validarRegistro() {
     // Limpiar mensajes de error
-    document.getElementById("errorPass").textContent = "";
-    document.getElementById("errorEmail").textContent = "";
     document.getElementById("errorNombre").textContent = "";
+    document.getElementById("errorEmail").textContent = "";
+    document.getElementById("errorPass").textContent = "";
+    document.getElementById("errorRepass").textContent = "";
     
     // Obtener valores de los campos
     const password = document.getElementById("password").value;
     const email = document.getElementById("email").value;
     const nombre = document.getElementById("nombre").value;
+    const repassword = document.getElementById("repassword").value;
     
     let hayErrores = false;
 
@@ -54,6 +56,18 @@ function validarRegistro() {
     if (password.trim() === "") {
         document.getElementById("errorPass").textContent = "El ingreso de contraseña es obligatorio.";
         document.getElementById("password").classList.add("input-error");
+        hayErrores = true;
+    }
+    if (repassword.trim() === "") {
+        document.getElementById("errorRepass").textContent = "El reingreso de contraseña es obligatorio.";
+        document.getElementById("password").classList.add("input-error");
+        hayErrores = true;
+    }
+
+    if (password !== repassword) {
+        document.getElementById("errorRepass").textContent = "Las contraseñas no coinciden.";
+        document.getElementById("password").classList.add("input-error");
+        document.getElementById("repassword").classList.add("input-error");
         hayErrores = true;
     }
 
